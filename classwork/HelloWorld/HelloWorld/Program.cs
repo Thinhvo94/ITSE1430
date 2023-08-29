cdf698c6fc7293fbbfacaca0ced7663264d6cd7e
thinh <votruongthinh1994@yahoo.com.vn>
2023-08-28 22:39:10 -0500

    Date 2 (08-24-23)

---
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Copyright Company
+ * Author
+ * Date
+ */
+
+
+class Program
+{
+    // Single line comment
+    // Author single line comment
+    /*
+     * Multi line cmment
+       ignored
+     */
+
+    static void Main()
+    {
+        Console.WriteLine("Enter your name: ");
+        string nanme = Console.ReadLine();
+
+        Console.WriteLine("Hello World " + nanme);
+    }
+}

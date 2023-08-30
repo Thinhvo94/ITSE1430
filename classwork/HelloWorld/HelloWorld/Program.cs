075cf106f3a8f6e57f09cf1e179bb66faaf91885
Truong Vo <truong.vo369@my.tccd.edu>
2023-08-29 19:16:11 -0500

    DAY 2

---
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Copyright Company
+ * author
+ * date
+ */
+
+class Program
+{
+    // Single line cmt
+    // Anther single line cmt
+    /*
+     * Multi line cmt
+     * ignored
+     */
+    static void Main()
+    {
+        //int x = "Bob"; --> will show the eror message
+
+        Console.WriteLine("Enter right name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hello World " + name);
+
+        
+
+        
+
+    }
+}
